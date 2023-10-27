3915c4108001d977608019c06a9656b12b6a9d4a
Gary Yu <hyu100@my.bcit.ca>
2023-10-27 11:20:35 -0700

    create counter and submit button

---
+++ src/app/counter/page.tsx
@@ -0,0 +1,10 @@
+import Counter from "./counter";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Counter</h1>
+      <Counter />
+    </div>
+  );
+}
--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -1,7 +1,28 @@
+import { db } from "@/db"
+import { posts } from "@/db/schema/posts"
+import {redirect} from 'next/navigation'
+import { revalidatePath } from "next/cache"
+
+import SubmitButton from "./submit-button"
+
 export default function CreatePost() {
+
+    async function handleCreatePost(data: FormData) {
+        "use server"
+        const content = data.get("content") as string
+        
+        const result = await db.insert(posts).values({
+            content,
+            userId: "1"
+        }).returning()
+        
+        revalidatePath("/")
+        redirect("/")
+    }
+
     return (
       <main className="text-center mt-10">
-        <form className="border border-neutral-500 rounded-lg px-6 py-4 flex flex-col gap-4">
+        <form action={handleCreatePost} className="border border-neutral-500 rounded-lg px-6 py-4 flex flex-col gap-4">
           <label className="w-full">
             <textarea
               className="bg-transparent flex-1 border-none outline-none w-full"
@@ -11,9 +32,7 @@ export default function CreatePost() {
             />
           </label>
   
-          <button type="submit" className="border rounded-xl px-4 py-2">
-            Post
-          </button>
+          <SubmitButton />
         </form>
       </main>
     )
